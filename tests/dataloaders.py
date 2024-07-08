9feeacea13e31904726c18845fa413aaa786ba87
zjykzj <wy163zhuj@163.com>
2024-07-08 21:38:18 +0800

    test(dataloaders.py/model.py): test dataset/dataloader/nms

--- tests/dataloaders.py
+++ tests/dataloaders.py
@@ -10,13 +10,15 @@
 import cv2
 import yaml
 
+from tqdm import tqdm
 import numpy as np
 
-from models.yolo5face.dataloaders import LoadImagesAndLabels
+from models.yolo5face.dataloaders import LoadImagesAndLabels, create_dataloader
+from utils.general import TQDM_BAR_FORMAT, colorstr
 
 
-def show_image(dataset):
-    for index in np.random.choice(len(dataset), size=10, replace=False):
+def show_image_by_dataset(dataset):
+    for index in np.random.choice(len(dataset), size=5, replace=False):
         img, labels, img_path, shapes = dataset.__getitem__(index)
 
         # CHW to HWC, RGB to BGR
@@ -48,12 +50,54 @@ def show_image(dataset):
         cv2.waitKey(0)
 
 
+def show_image_by_dataloader(dataloader):
+    total = 5
+
+    nb = len(dataloader)  # number of batches
+    pbar = enumerate(dataloader)
+    pbar = tqdm(pbar, total=total, bar_format=TQDM_BAR_FORMAT)  # progress bar
+    for i, (imgs, targets, paths, _) in pbar:  # batch -------------------------------------------------------------
+        for idx, img in enumerate(imgs):
+            labels = targets[targets[:, 0] == idx]
+
+            # CHW to HWC, RGB to BGR
+            img = img.numpy().transpose((1, 2, 0))[..., ::-1]
+            img = np.ascontiguousarray(img)
+
+            img_h, img_w = img.shape[:2]
+            # print(f"img type: {type(img)} - dtype: {img.dtype} - shape: {img.shape}")
+
+            for item in labels.numpy():
+                x_c, y_c, box_w, box_h = item[2:6]
+                x_min = (x_c - box_w / 2) * img_w
+                y_min = (y_c - box_h / 2) * img_h
+                x_max = (x_c + box_w / 2) * img_w
+                y_max = (y_c + box_h / 2) * img_h
+
+                cv2.rectangle(img, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 1)
+
+                for kp in item[6:].reshape(-1, 2):
+                    x, y = kp
+                    if x < 0 or y < 0:
+                        continue
+                    x = int(x * img_w)
+                    y = int(y * img_h)
+
+                    cv2.circle(img, (x, y), 1, (0, 0, 255), -1)
+
+            cv2.imshow("img", img)
+            cv2.waitKey(0)
+        if (i + 1) >= total:
+            break
+
+
 def create_dataset(is_train=False):
     hyp = "../models/yolo5face/hyps/hyp.scratch-low.yaml"
     with open(hyp, errors='ignore') as f:
         hyp = yaml.safe_load(f)  # load hyps dict
 
-    path = '/home/zj/datasets/widerface/images/train'
+    # path = '/home/zj/datasets/widerface/images/train'
+    path = r'C:\zj\repos\datasets\widerface\images\train'
     dataset = LoadImagesAndLabels(path, img_size=640, batch_size=1, augment=is_train, hyp=hyp)
 
     return dataset
@@ -63,14 +107,54 @@ def test_dataset():
     print('*' * 100 + " Eval")
     dataset = create_dataset(is_train=False)
     print(dataset)
-    show_image(dataset)
+    show_image_by_dataset(dataset)
 
     print('*' * 100 + " Train")
     dataset = create_dataset(is_train=True)
     print(dataset)
-    show_image(dataset)
+    show_image_by_dataset(dataset)
+
+
+def test_dataloader():
+    hyp = "../models/yolo5face/hyps/hyp.scratch-low.yaml"
+    with open(hyp, errors='ignore') as f:
+        hyp = yaml.safe_load(f)  # load hyps dict
+
+    imgsz = 640
+    batch_size = 1
+    stride = 32
+
+    print('*' * 100 + " Eval")
+    # valloader
+    val_path = r'C:\zj\repos\datasets\widerface\images\train'
+    val_loader = create_dataloader(val_path,
+                                   imgsz,
+                                   batch_size,
+                                   stride,
+                                   single_cls=False,
+                                   hyp=hyp,
+                                   rect=True,
+                                   rank=-1,
+                                   workers=2,
+                                   pad=0.5,
+                                   prefix=colorstr('val: '))[0]
+    show_image_by_dataloader(val_loader)
+
+    print('*' * 100 + " Train")
+    # Trainloader
+    train_path = r'C:\zj\repos\datasets\widerface\images\train'
+    train_loader, dataset = create_dataloader(train_path,
+                                              imgsz,
+                                              batch_size,
+                                              stride,
+                                              single_cls=False,
+                                              hyp=hyp,
+                                              augment=True,
+                                              prefix=colorstr('train: '),
+                                              shuffle=True)
+    show_image_by_dataloader(train_loader)
 
 
 if __name__ == '__main__':
-    # create_dataset()
     test_dataset()
+    test_dataloader()
--- tests/model.py
+++ tests/model.py
@@ -4,44 +4,56 @@
 @date: 2024/6/23 下午8:14
 @file: model.py
 @author: zj
-@description: 
+@description:
+
 """
 
+import cv2
+import numpy as np
 import torch
 
-from models.yolo import Model
-from models.common import Conv
-from models.yolo5face.comonents import StemBlock
+from models.yolo5face.general import non_max_suppression, scale_landmarks
+from utils.general import TQDM_BAR_FORMAT, colorstr, scale_boxes
 
 
-def test1():
-    model1 = Conv(3, 64, 6, 2, 2)
-    model2 = StemBlock(3, 64, 3, 2, )
+def test_nms():
+    pred = np.load("../assets/widerface/0_Parade_marchingband_1_1004/nms_before.npy")
+    pred = np.concatenate((pred[..., :5], pred[..., -1:], pred[..., 5:-1]), axis=-1)
+    print(f"pred shape: {pred.shape}")
 
-    data = torch.randn(1, 3, 640, 640)
-    res1 = model1(data)
-    res2 = model2(data)
-    print(res1.shape, res2.shape)
+    img1_shape = (512, 640)
+    conf_thres = 0.25
+    iou_thres = 0.45
+    pred = non_max_suppression(torch.from_numpy(pred),
+                               conf_thres,
+                               iou_thres,
+                               labels=(),
+                               multi_label=False,
+                               agnostic=False,
+                               max_det=300)
+    print(len(pred[0]), 'face' if len(pred[0]) == 1 else 'faces')
 
+    img_path = "../assets/widerface/0_Parade_marchingband_1_1004.jpg"
+    img = cv2.imread(img_path)
+    print(f"img type: {type(img)} - dtype: {img.dtype} - shape: {img.shape}")
 
-def test2():
-    cfg1 = "models/yolo5face/cfgs/yolov5s_face.yaml"
-    model1 = Model(cfg1, ch=3, nc=80, anchors=None)  # create
-    model1.train()
-    print(model1)
+    det = pred[0]
+    det[:, :4] = scale_boxes(img1_shape, det[:, :4], img.shape[:2]).round()
+    det[:, 6:] = scale_landmarks(img1_shape, det[:, 6:], img.shape[:2]).round()
+    for item in det:
+        print(item)
+        x_min, y_min, x_max, y_max = item[:4]
+        cv2.rectangle(img, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 1)
 
-    cfg2 = "models/yolov5s.yaml"
-    model2 = Model(cfg2, ch=3, nc=80, anchors=None)  # create
-    model2.train()
-    print(model2)
+        for kp in item[6:].reshape(-1, 2):
+            x, y = kp
+            if x < 0 or y < 0:
+                continue
+            cv2.circle(img, (int(x), int(y)), 1, (0, 0, 255), -1)
 
-    data = torch.randn(1, 3, 640, 640)
-    res1 = model1(data)
-    res2 = model2(data)
-    for item1, item2 in zip(res1, res2):
-        print(item1.shape, item2.shape)
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-    test1()
-    test2()
+    test_nms()
