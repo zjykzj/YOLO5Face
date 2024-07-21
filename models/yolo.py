b8cb3adbf09754307b027fbea5d725a1f3263fed
zjkjzj <wy163zhuj@163.com>
2024-07-21 13:07:03 +0800

    feat(models/yolo5face/cfgs/shufflev2-face.yaml): add FaceDetector based on ShuffleNetV2

--- models/yolo.py
+++ models/yolo.py
@@ -29,7 +29,7 @@ from utils.plots import feature_visualization
 from utils.torch_utils import (fuse_conv_and_bn, initialize_weights, model_info, profile, scale_img, select_device,
                                time_sync)
 
-from models.yolo5face.comonents import StemBlock, FaceDetect
+from models.yolo5face.comonents import StemBlock, FaceDetect, ShuffleV2Block
 
 try:
     import thop  # for FLOPs computation
@@ -316,9 +316,9 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 args[j] = eval(a) if isinstance(a, str) else a  # eval strings
 
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in {
-            Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
-            BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, StemBlock}:
+        if m in {Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
+                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x,
+                 StemBlock, ShuffleV2Block}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
