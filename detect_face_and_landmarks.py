938466d213f5a54979602273f52eb3bdb960c5bb
zjkjzj <wy163zhuj@163.com>
2024-08-17 21:58:38 +0800

    style(detect_face_and_landmarks.py): update Pytorch/ONNXRuntime usage

--- detect_face_and_landmarks.py
+++ detect_face_and_landmarks.py
@@ -2,28 +2,12 @@
 """
 Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, webcam, streams, etc.
 
-Usage - sources:
-    $ python detect.py --weights yolov5s.pt --source 0                               # webcam
-                                                     img.jpg                         # image
-                                                     vid.mp4                         # video
-                                                     screen                          # screenshot
-                                                     path/                           # directory
-                                                     'path/*.jpg'                    # glob
-                                                     'https://youtu.be/Zgi9g1ksQHc'  # YouTube
-                                                     'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
+Usage - Predict Plates using Pytorch:
+    $ python3 detect_face_and_landmarks.py --weights shufflev2_face-i800-e300.pt --source ./assets/widerface/
 
-Usage - formats:
-    $ python detect.py --weights yolov5s.pt                 # PyTorch
-                                 yolov5s.torchscript        # TorchScript
-                                 yolov5s.onnx               # ONNX Runtime or OpenCV DNN with --dnn
-                                 yolov5s_openvino_model     # OpenVINO
-                                 yolov5s.engine             # TensorRT
-                                 yolov5s.mlmodel            # CoreML (macOS-only)
-                                 yolov5s_saved_model        # TensorFlow SavedModel
-                                 yolov5s.pb                 # TensorFlow GraphDef
-                                 yolov5s.tflite             # TensorFlow Lite
-                                 yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
-                                 yolov5s_paddle_model       # PaddlePaddle
+Usage - Predict Plates using ONNXRuntime:
+    $ python3 detect_face_and_landmarks.py --weights shufflev2_face-i800-e300.onnx --source ./assets/widerface/
+
 """
 
 import argparse
